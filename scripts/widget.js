e6a833de473d87499364fac7eda3fcd7a2f5acd5
arqu33n <soondawn@yandex.ru>
2023-06-21 15:05:30 +0300

    add API connecting

--- scripts/widget.js
+++ scripts/widget.js
@@ -1,6 +1,7 @@
+import { fetchWeather } from "./modules/APIservice.js";
 import { startWidget } from "./modules/widgetService.js";
-const initWidget = (app) => {
-  const widget = startWidget();
+const initWidget = async (app) => {
+  const widget = await startWidget();
   app.append(widget);
 };
 initWidget(document.querySelector("#app"));
