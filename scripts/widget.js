757a01fc36408a1ea460837b7e0544075ef2483c
arqu33n <soondawn@yandex.ru>
2023-06-22 14:31:32 +0300

    add forecast to widget

--- scripts/widget.js
+++ scripts/widget.js
@@ -1,4 +1,3 @@
-import { fetchWeather } from "./modules/APIservice.js";
 import { startWidget } from "./modules/widgetService.js";
 const initWidget = async (app) => {
   const widget = await startWidget();
